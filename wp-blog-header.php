a6a5e77cf2113d83d3bf90418e72be03ea2f22d6
David Ibáñez <dibanez@gmail.com>
2016-05-25 14:14:08 +0200

    arreglo ip

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -14,7 +14,7 @@ if ( !isset($wp_did_header) ) {
 
 	// Set up the WordPress query.
 	wp();
-	echo $_SERVER['SERVER_ADDR'];
+	
 	// Load the theme template.
 	require_once( ABSPATH . WPINC . '/template-loader.php' );
 
